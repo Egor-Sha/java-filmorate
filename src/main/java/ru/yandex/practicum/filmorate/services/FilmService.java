a11301bc1c6244e73a53dc8c4fd18423d677dc70
egorshalaev <shalaev.e@ya.ru>
2022-10-24 19:19:09 +0200

    10th sprint: notnull check

--- src/main/java/ru/yandex/practicum/filmorate/services/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/services/FilmService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.DataNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
@@ -17,61 +18,63 @@ public class FilmService {
     private final LocalDate FIRST_MOVIE_DATE = LocalDate.of(1895, 12, 29);
     Comparator<Film> FILM_COMPARATOR = Comparator.comparingLong(Film::getRate);
     Storage<User> userStorage;
-    Storage<Film> storage;
+    Storage<Film> filmStorage;
     private long counter = 0L;
 
     @Autowired
     public FilmService(Storage<Film> storage, Storage<User> userStorage) {
-        this.storage = storage;
+        this.filmStorage = filmStorage;
         this.userStorage = userStorage;
     }
 
     public Film create(Film data) {
         validate(data);
         data.setId(++counter);
-        storage.create(data);
+        filmStorage.create(data);
         return data;
     }
 
     public Film get(long id) {
-        return storage.get(id);
+        return filmStorage.get(id);
     }
 
     public Film update(Film data) {
-        storage.get(data.getId());
+        filmStorage.get(data.getId());
         data.setId(data.getId());
         validate(data);
-        storage.update(data);
+        filmStorage.update(data);
         return data;
     }
 
     public List<Film> getAll() {
-        return storage.getAll();
+        return filmStorage.getAll();
     }
 
     protected void validate(Film data) {
         if (data.getReleaseDate().isBefore(FIRST_MOVIE_DATE)) {
             throw new ValidationException("Проверьте дату релиза");
         }
-        if (data.getDescription().length()>200) {
+        if (data.getDescription().length()>=200) {
             throw new ValidationException("Описание должно быть до 200 символов");
         }
     }
 
-    public void addLike(long id, long userId) {
-        final Film film = storage.get(id);
-        userStorage.get(userId);
-        film.addLike(userId);
+    public void addLike(long id, long userId) throws DataNotFoundException {
+        final Film film = filmStorage.get(id);
+        if (film == null) {throw new DataNotFoundException("Film not found");}
+        if (userStorage.get(userId) == null) {throw new DataNotFoundException("User not found");}
+        film.addLike(id);
     }
 
     public void removeLike(long id, long userId) {
-        final Film film = storage.get(id);
-        userStorage.get(userId);
-        film.removeLike(userId);
+        final Film film = filmStorage.get(id);
+        if (film == null) {throw new DataNotFoundException("Film not found");}
+        if (userStorage.get(userId) == null) {throw new DataNotFoundException("User not found");}
+        film.removeLike(id);
     }
 
     public List<Film> getPopular(int count) {
-        return storage.getAll().stream()
+        return filmStorage.getAll().stream()
                 .sorted(FILM_COMPARATOR)
                 .limit(count)
                 .collect(Collectors.toList());
--- src/main/java/ru/yandex/practicum/filmorate/services/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/services/UserService.java
@@ -3,7 +3,9 @@ package ru.yandex.practicum.filmorate.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import ru.yandex.practicum.filmorate.exceptions.DataNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.Storage;
 import java.util.HashSet;
@@ -13,35 +15,35 @@ import java.util.Set;
 @Service
 public class UserService {
 
-    Storage<User> storage;
+    Storage<User> userStorage;
     private long counter = 0L;
 
     @Autowired
     public UserService(Storage<User> storage) {
-        this.storage = storage;
+        this.userStorage = storage;
     }
 
     public User create(User data) {
         validate(data);
         data.setId(++counter);
-        storage.create(data);
+        userStorage.create(data);
         return data;
     }
 
     public User get(long id) {
-        return storage.get(id);
+        return userStorage.get(id);
     }
 
     public User update(User data) {
-        storage.get(data.getId());
+        userStorage.get(data.getId());
         data.setId(data.getId());
         validate(data);
-        storage.update(data);
+        userStorage.update(data);
         return data;
     }
 
     public List<User> getAll() {
-        return storage.getAll();
+        return userStorage.getAll();
     }
 
     protected void validate(User data) {
@@ -53,29 +55,32 @@ public class UserService {
         }
     }
 
-    public void addFriend(long userId, long friendId) {
-        final User user = storage.get(userId);
-        final User friend = storage.get(friendId);
+    public void addFriend(long userId, long friendId) throws DataNotFoundException {
+        final User user = userStorage.get(userId);
+        final User friend = userStorage.get(userId);
+        if (user == null) {throw new DataNotFoundException("User not found");}
+        if (friend == null) {throw new DataNotFoundException("Friend not found");}
         user.getFriendsId().add(friendId);
         friend.getFriendsId().add(userId);
     }
 
-    public void removeFriend(long userId, long friendId) {
-        final User user = storage.get(userId);
-        final User friend = storage.get(friendId);
-        storage.get(friendId);
+    public void removeFriend(long userId, long friendId) throws DataNotFoundException {
+        final User user = userStorage.get(userId);
+        final User friend = userStorage.get(userId);
+        if (user == null) {throw new DataNotFoundException("User not found");}
+        if (friend == null) {throw new DataNotFoundException("Friend not found");}
         user.getFriendsId().remove(friendId);
         friend.getFriendsId().remove(userId);
     }
 
     public Set<Long> getFriends(long userId) {
-        final User user = storage.get(userId);
+        final User user = userStorage.get(userId);
         return user.getFriendsId();
     }
 
     public Set<Long> getCommonFriends(long userId, long friendId) {
-        final User user = storage.get(userId);
-        final User friend = storage.get(friendId);
+        final User user = userStorage.get(userId);
+        final User friend = userStorage.get(friendId);
         Set<Long> common = new HashSet<>();
         common.addAll(user.getFriendsId());
         common.addAll(friend.getFriendsId());
